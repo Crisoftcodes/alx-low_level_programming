0015e2e352f53c8f7e23ddc0b6a3b016402baca6
Christian Ojiegbe <ojiegbe2christian@gmail.com>
2022-09-17 17:52:57 -0700

    c programming

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ *  main - Entry point
+ *  
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Hello World/n");
+	printf("Hello");
 	    return (0);
 }
