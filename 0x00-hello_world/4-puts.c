1d8f9ea26df1d64bada220ab8ef6b36dccd8db96
Christian Ojiegbe <ojiegbe2christian@gmail.com>
2022-09-20 17:46:47 -0700

    Using the puts function to print out string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Using the puts function
+ * this code prints the string in puts function
+ * and returns 0
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
